c5e407838396b534aff84b875efc956bc82c87b4
mas0110 <masoudmohammed13@gmail.com>
2024-04-08 15:40:57 +0200

    models di category tags e post

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\support\Str;
 
 class Post extends Model
@@ -13,11 +15,27 @@ class Post extends Model
     protected $fillable = [
         'title',
         'content',
-        'slug'
+        'slug',
+        'cover_image',
+        'category_id'
     ];
 
     public static function generateSlug($title)
     {
         return Str::slug($title, '-');
+    }
+
+    //un post può avere una sola categoria
+
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo( Category::class );
+    }
+
+    //un singolo post è collegato a più tags
+
+    public function tags(): BelongsToMany
+    {
+        return $this->belongsToMany(Tag::class);
     }
 }
